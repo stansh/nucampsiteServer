0f1899c1571e9b067e0c911d1c5679d70fa99bf1
Stan S <stan.shm@gmail.com>
2020-03-25 20:32:29 -0500

    Cookies

--- app.js
+++ app.js
@@ -37,31 +37,43 @@ app.set('view engine', 'jade');
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
+app.use(cookieParser('12345-67890-09876-54321')); // cookie secret key to "sign" cooke
 
 
 //Authentication middleware
 
 function auth(req, res, next) {
-  console.log(req.headers);
-  const authHeader = req.headers.authorization;
-  if (!authHeader) { //checks if authorization header is empty
-      const err = new Error('You are not authenticated!');
-      res.setHeader('WWW-Authenticate', 'Basic'); //requesting authentication with the type Basic
-      err.status = 401;
-      return next(err); //Express deales with the error and also challenges the client to provide correct credentilas (to create authorization header)
-  }
- //authorization header is provided
-  const auth = Buffer.from(authHeader.split(' ')[1], 'base64').toString().split(':'); //Buffer parses the authorization header into a 2 item array (from base 64 encoded stream format)
-  const user = auth[0];
-  const pass = auth[1];
-  if (user === 'admin' && pass === 'password') {
-      return next(); // authorized
+    if (!req.signedCookies.user) { // signedCookies property of the request object is provided by cookie parser; signed cookie is parsed; property USER will be added to cookie
+        console.log(req.headers);
+        const authHeader = req.headers.authorization;
+        if (!authHeader) { //checks if authorization header is empty
+            const err = new Error('You are not authenticated!');
+            res.setHeader('WWW-Authenticate', 'Basic'); //requesting authentication with the type Basic
+            err.status = 401;
+            return next(err); //Express deales with the error and also challenges the client to provide correct credentilas (to create authorization header)
+        }
+      //authorization header is provided
+        const auth = Buffer.from(authHeader.split(' ')[1], 'base64').toString().split(':'); //Buffer parses the authorization header into a 2 item array (from base 64 encoded stream format)
+        const user = auth[0];
+        const pass = auth[1];
+        if (user === 'admin' && pass === 'password') {
+            res.cookie('user', 'admin', {signed: true}); // new cookie created; cookie's property USER is added;  {signed: true} lets know to use the secret key to SIGN cookie
+            return next(); // authorized
+        } else {
+            const err = new Error('You are not authenticated!');
+            res.setHeader('WWW-Authenticate', 'Basic');      
+            err.status = 401;
+            return next(err);
+        }
+
   } else {
-      const err = new Error('You are not authenticated!');
-      res.setHeader('WWW-Authenticate', 'Basic');      
-      err.status = 401;
-      return next(err);
+    if (req.signedCookies.user === 'admin') {  
+        return next();  //access granted from the server
+    } else {
+        const err = new Error('You are not authenticated!');
+        err.status = 401;
+        return next(err);
+    }
   }
 }
 
